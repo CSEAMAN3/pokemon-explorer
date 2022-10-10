01ddd6e9d451e3c5c30b405824cfecf73c33d7f0
Chris Seaman <christopherseaman1983@gmail.com>
2022-10-10 09:04:26 +0100

    footer updated, github icon and copyright heading

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Footer.css";
+import githubLogo from "../../images/github-icon.svg";
 
 export default function Footer() {
   return (
     <footer>
       <div className="footer-container">
-        <h1>This is a footer</h1>
+        <h1 className="footer-heading">&#169; Christoher Seaman : Pokemon Explorer</h1>
+        <div className="triangle"></div>
+        <img className="github-icon" src={githubLogo} alt="github logo link to github profile" />
       </div>
     </footer>
   );

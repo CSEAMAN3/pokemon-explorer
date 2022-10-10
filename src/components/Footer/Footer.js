17f3d0407fad3168cc793cbc62ea5f658fbe515a
Chris Seaman <christopherseaman1983@gmail.com>
2022-10-10 09:18:18 +0100

    container margin bottom 1rem added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="footer-container">
-        <h1 className="footer-heading">&#169; Christoher Seaman : Pokemon Explorer</h1>
+        <h1 className="footer-heading">&#169; Christopher Seaman Pokemon Explorer</h1>
         <div className="triangle"></div>
         <img className="github-icon" src={githubLogo} alt="github logo link to github profile" />
       </div>

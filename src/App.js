8f6975116bc7a0e81fb252e59f9c0db1a8b6fc1e
Chris Seaman <christopherseaman1983@gmail.com>
2022-10-12 17:07:10 +0100

    battle arena player one and player two added

--- src/App.js
+++ src/App.js
@@ -15,10 +15,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   const [pokemon, setPokemon] = useState({});
   const [form, setForm] = useState({ name: "" });
+  const [playerOne, setPlayerOne] = useState({});
+  const [playerTwo, setPlayerTwo] = useState({});
+
+  const getPlayer1Pokemon = async (event) => {
+    event.preventDefault();
+    const API = `https://pokeapi.co/api/v2/pokemon/${form.name.toLowerCase()}`;
+    const res = await axios.get(API);
+    setPlayerOne(res.data);
+  };
+
+  const getPlayer2Pokemon = async (event) => {
+    event.preventDefault();
+    const API = `https://pokeapi.co/api/v2/pokemon/${form.name.toLowerCase()}`;
+    const res = await axios.get(API);
+    setPlayerTwo(res.data);
+  };
 
   const handleChange = (event) => {
     setForm({ ...form, [event.target.name]: event.target.value });
-    console.log(event.target.value);
   };
 
   const getPokemon = async (event) => {
@@ -37,7 +52,19 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home handleChange={handleChange} getPokemon={getPokemon} pokemon={pokemon} />} />
-            <Route path="/battle-arena" element={<BattleArena />} />
+            <Route
+              path="/battle-arena"
+              element={
+                <BattleArena
+                  handleChange={handleChange}
+                  getPlayer1Pokemon={getPlayer1Pokemon}
+                  getPlayer2Pokemon={getPlayer2Pokemon}
+                  // pokemon={pokemon}
+                  playerOne={playerOne}
+                  playerTwo={playerTwo}
+                />
+              }
+            />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
--- src/pages/BattleArena.js
+++ src/pages/BattleArena.js
@@ -1,5 +1,57 @@
 import React from "react";
+import "./BattleArena.css";
 
-export default function BattleArena() {
-  return <div>BattleArena</div>;
+export default function BattleArena({ getPlayer1Pokemon, getPlayer2Pokemon, handleChange, playerOne, playerTwo }) {
+  return (
+    <div className="battle-arena">
+      <div className="battle-arena-container">
+        <h2 className="welcome-heading">Welcome to pokemon Battle Arena. Battle pokemon, see who wins!</h2>
+
+        <div className="pokemon-container">
+          <div className="player-one-container">
+            <form className="player-one-form" onSubmit={getPlayer1Pokemon}>
+              <input
+                className="search-input"
+                onChange={handleChange}
+                placeholder="Player 1 pokemon"
+                name="name"
+                type="text"
+              />
+              <button className="player1-btn">Confirm</button>
+            </form>
+            <div className={`player-one-content-container ${playerOne.abilities ? "" : "hide"}`}>
+              <h3 className="player-one-pokemon-name">{playerOne.name}</h3>
+              <img
+                className="player1-img"
+                src={playerOne.sprites?.versions?.["generation-v"]["black-white"].animated.front_default}
+                alt="playerOne.name"
+              />
+            </div>
+          </div>
+
+          <div className="player-two-container">
+            <form className="player-two-form" onSubmit={getPlayer2Pokemon}>
+              <input
+                className="search-input"
+                onChange={handleChange}
+                placeholder="Player 2 pokemon"
+                name="name"
+                type="text"
+              />
+              <button className="player2-btn">Confirm</button>
+            </form>
+            <div className={`player-two-content-container ${playerTwo.abilities ? "" : "hide"}`}>
+              <h3 className="player-two-pokemon-name">{playerTwo.name}</h3>
+              <img
+                className="player2-img"
+                src={playerTwo.sprites?.versions?.["generation-v"]["black-white"].animated.front_default}
+                alt="playerTwo.name"
+              />
+            </div>
+          </div>
+        </div>
+        <button className="battle-btn">battle</button>
+      </div>
+    </div>
+  );
 }
